f4055451c46078c435d9c9f470807eb71cfeb14b
currycafe <k022g1028@g.neec.ac.jp>
2023-06-26 15:45:42 +0900

    02_14_確認課題完了

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -18,7 +18,7 @@ void RailCamera::Initialize(Vector3 translation, Vector3 rotation)
 void RailCamera::Update()
 {
 	Vector3 move = { 0.0f,0.0f,0.0f };
-	Vector3 rotate = { 0.0f,0.0f,0.0f };
+	Vector3 rotate = { 0.0f,0.001f,0.0f };
 	worldTransform_.translation_.x += move.x;
 	worldTransform_.translation_.y += move.y;
 	worldTransform_.translation_.z += move.z;
